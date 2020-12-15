8e737d86939c7d4cc73bd1d1510c5be388c24ba3
NickBwalley <nickbwalley@gmail.com>
2020-12-15 23:26:48 +0300

    oopConcepts

--- src/caseStudy/LoansAccount.java
+++ src/caseStudy/LoansAccount.java
@@ -10,3 +10,12 @@ public class LoansAccount extends BankAccount //inheritance
 		System.out.println("Object is destroyed by the CurrentAccount Class!..\n");
 	}
 }
+
+
+/*VERY IMPORTANT DEFINITIONS!..
+ * Polymorphism: The ability of a message to be displayed in more than one form.
+ * Encapsulation: Java mechanism of wrapping data (variables) and code acting on the data (methods) together as a single unit.
+ * Abstraction: This is the process of hiding the implementation details from the user, only the functionality will be provided to the user
+ * Inheritance: This is the mechanism in which one class acquires the property of another class. 
+ * Overloading: Occurs when two or more methods in one class have the same methods name but different parameters.
+ * Overriding: Occurs when two methods have the same method name and parameters.*/
--- src/caseStudy/SavingsAccount.java
+++ src/caseStudy/SavingsAccount.java
@@ -4,20 +4,7 @@ public class SavingsAccount extends BankAccount //inheritance
 {
 	private double balance;
 	private double interest;
-	//private String name;
-	//private int accNo;
-    //private final int PIN = 1234; 
-    //private int PIN2;
-	/*
-	 * public void pinChecker() { Scanner input = new Scanner(System.in);
-	 * System.out.println("******************************");
-	 * System.out.println("I-LAB SAVINGS ACCOUNT!..");
-	 * System.out.println("Enter Account No: "); accNo = input.nextInt(); do {
-	 * System.out.println("Enter PIN: "); PIN2 = input.nextInt(); }while(PIN2 !=
-	 * PIN);
-	 * 
-	 * }
-	 */
+	
 	public SavingsAccount() // this is a constructor
 	{
 		balance = 0;
@@ -50,9 +37,18 @@ public class SavingsAccount extends BankAccount //inheritance
 		return balance;
 	}
 	
-	protected void finalize()
-	{	//This is a de-constructor
+	protected void finalize()	//This is a de-constructor
+	{	
 		System.out.println("Object is destroyed by the CurrentAccount Class!..\n");
 	}
 
 }
+
+
+/*VERY IMPORTANT DEFINITIONS!..
+ * Polymorphism: The ability of a message to be displayed in more than one form.
+ * Encapsulation: Java mechanism of wrapping data (variables) and code acting on the data (methods) together as a single unit.
+ * Abstraction: This is the process of hiding the implementation details from the user, only the functionality will be provided to the user
+ * Inheritance: This is the mechanism in which one class acquires the property of another class. 
+ * Overloading: Occurs when two or more methods in one class have the same methods name but different parameters.
+ * Overriding: Occurs when two methods have the same method name and parameters.*/
