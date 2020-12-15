8d1778191335e9f6a196ac1ee900f36b0f195928
NickBwalley <nickbwalley@gmail.com>
2020-12-15 23:18:24 +0300

    banking system

--- src/caseStudy/CurrentAccount.java
+++ src/caseStudy/CurrentAccount.java
@@ -3,25 +3,10 @@ import java.util.Scanner;
 public class CurrentAccount extends BankAccount //inheritance
 {
 	private double balance = 400; //current balance stored in the database. The system computes and updates after user transaction
-	//private String name;
-	//private int accNo;
-    //private final int PIN = 1234; //default PIN  stored in the database can be manipulated by the user
-    //private int PIN2;
     private double deposit;
     private double withdraw;
     Scanner input = new Scanner(System.in);
 
-	/*
-	 * public void pinChecker() {
-	 * 
-	 * System.out.println("*******************************");
-	 * System.out.println("I-LAB CURRENT ACCOUNT!..");
-	 * System.out.println("Enter Account No: "); accNo = input.nextInt(); do {
-	 * System.out.println("Enter PIN: "); PIN2 = input.nextInt(); }while(PIN2 !=
-	 * PIN); //input.close();
-	 * 
-	 * }
-	 */
 	public CurrentAccount() // this is a constructor
 	{
 		//balance = 0;
@@ -60,3 +45,12 @@ public class CurrentAccount extends BankAccount //inheritance
 	}
 
 }
+
+/*VERY IMPORTANT DEFINITIONS!..
+ * Polymorphism: The ability of a message to be displayed in more than one form.
+ * Encapsulation: Java mechanism of wrapping data (variables) and code acting on the data (methods) together as a single unit.
+ * Abstraction: This is the process of hiding the implementation details from the user, only the functionality will be provided to the user
+ * Inheritance: This is the mechanism in which one class acquires the property of another class. 
+ * Overloading: Occurs when two or more methods in one class have the same methods name but different parameters.
+ * Overriding: Occurs when two methods have the same method name and parameters.*/
+ */
